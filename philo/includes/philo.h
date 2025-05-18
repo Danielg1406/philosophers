dc0954b2041431d7792a95b106c8c5a641d7734e
Daniel Gomez Arango <dgomez-a@student.42berlin.de>
2025-05-18 19:32:41 +0200

    initial project structure

---
+++ philo/includes/philo.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgomez-a <dgomez-a@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/18 18:09:26 by dgomez-a          #+#    #+#             */
+/*   Updated: 2025/05/18 19:32:33 by dgomez-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+
+typedef struct s_philosopher
+{
+	int	id;
+	int	flag_dead;
+	pthread_t	thread;
+	pthread_mutex_t	fork;	
+	t_philosopher	*next;
+	t_philosopher	*prev;
+}		t_philosopher;
+
+
