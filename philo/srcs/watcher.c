8dba384b83b15894f4756d0f35ea354ab972980a
Daniel Gomez Arango <dgomez-a@student.42berlin.de>
2025-06-25 14:58:35 +0200

    chore:add headers

--- philo/srcs/watcher.c
+++ philo/srcs/watcher.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   watcher.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgomez-a <dgomez-a@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 14:56:39 by dgomez-a          #+#    #+#             */
+/*   Updated: 2025/06/25 14:57:35 by dgomez-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
 void	*watcher_routine(void *arg)
@@ -13,7 +25,7 @@ void	*watcher_routine(void *arg)
 		{
 			if ((now_ms() - table->philos[i].last_meal) > table->time_to_die)
 			{
-        print_status(table, table->philos[i].id, "died");
+				print_status(table, table->philos[i].id, "died");
 				table->flag_dead = 1;
 				return (NULL);
 			}
